3230aef01f469ab4e0444952df1aa45ac799ce6d
Your Name <you@example.com>
2024-05-09 00:26:43 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -106,7 +106,7 @@ void removeNode(TreeMap * tree, TreeNode* node) {
             node->parent->right = NULL;
     }
     else{
-        if(node->left != NULL && node->right != NULL){
+        if(node->left != NULL && node->right == NULL){
             if(node->parent->left == node){
                 node->parent->left = node->left;
                 node->left->parent = node->parent;
